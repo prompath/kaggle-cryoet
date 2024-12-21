37918635ce772458de2833743765b0340d702fee
Jeerawat Chuachan <jeerawatchu@gmail.com>
2024-12-21 09:28:12 +0700

    feat: add config handling

--- kaggle_cryoet/config.py
+++ kaggle_cryoet/config.py
@@ -1,16 +1,20 @@
 """
 This module handles config loading.
 """
-# %%
+
+import json
 import os
+from pathlib import Path
 import tomllib
 
-def load_config(config_path: os.PathLike) -> dict:
-    with open(config_path, "rb") as f:
+
+def load_configs(config_dir: os.PathLike = "../configs") -> dict:
+    config_dir = Path(config_dir).resolve()
+    # Load project config
+    with open(config_dir / "config.toml", "rb") as f:
         config = tomllib.load(f)
+    # Load copick config
+    with open(config_dir / "copick.json", "rb") as f:
+        copick_json = json.load(f)
+    config["COPICK"] = copick_json
     return config
-
-# %%
-config = load_config("../config.toml")
-
-# %%
